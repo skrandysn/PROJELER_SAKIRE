00dce47176845b152e634a0e71ed335b425d8c34
skrandysn <sakiresolmaz@gmail.com>
2023-11-17 14:59:04 +0300

    REACT

---
+++ REACT/R-1/myapp/src/App.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Button from "./components/Button";
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hi React</h1>
+      <Button />
+    </div>
+  );
+};
+
+export default App;
---
+++ REACT/R-1/myapp/src/components/Button.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+
+const Button = () => {
+  return (
+    <div>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <button>Button</button>
+      <input type="checkbox" name="" id="" />
+    </div>
+  );
+};
+
+export default Button;
