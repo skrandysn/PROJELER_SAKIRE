eff18124e90003daa0086961df8861672e6879f8
skrandysn <sakiresolmaz@gmail.com>
2023-11-26 21:13:29 +0300

    r

--- REACT/r-4.horoscope/src/components/Main/Card.jsx
+++ REACT/r-4.horoscope/src/components/Main/Card.jsx
@@ -1,21 +1,21 @@
 import React from "react";
 
 const Card = (data) => {
-  // {title,date,desc,image}
-  return (
-    <div className="cards">
-      <div className="title">
-        <h1>{data.title}</h1>
-      </div>
-      <div className="date">{data.date}</div>
-      <div>
-        <img src={data.image} alt="card" />
-      </div>
-      <div className="card-over">
-        <p>{data.desc}</p>
-      </div>
-    </div>
-  );
+	// {title,date,desc,image} //* data böylede çekilebilir
+	return (
+		<div className="cards">
+			<div className="title">
+				<h1>{data.title}</h1>
+			</div>
+			<div className="date">{data.date}</div>
+			<div>
+				<img src={data.image} alt="card" />
+			</div>
+			<div className="card-over">
+				<p>{data.desc}</p>
+			</div>
+		</div>
+	);
 };
 
 export default Card;
