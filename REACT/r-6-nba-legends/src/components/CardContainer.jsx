c776c3cf476591cf8a24bb07a70d7887f1ed3927
skrandysn <sakiresolmaz@gmail.com>
2023-11-27 12:40:34 +0300

    degisim

--- REACT/r-6-nba-legends/src/components/CardContainer.jsx
+++ REACT/r-6-nba-legends/src/components/CardContainer.jsx
@@ -1,22 +1,34 @@
-import React from "react";
-import { Col, Container, Row } from "react-bootstrap";
-import { data } from "../helpers/data";
-import PlayerCard from "./PlayerCard";
+import React from 'react'
+import { Col, Container, Form, Row } from 'react-bootstrap'
+import {data} from '../helpers/data'
+import PlayerCard from './PlayerCard'
+import { useState } from 'react'
+
 
 const CardContainer = () => {
-	return (
-  <Container className="card-container rounded-4 my-4 p-3">
-    <Row className="g-3 justify-content-center">
-      {data.map((player) => (
-        <Col md={6} lg={4} xl={3}>
+    const [search, setSearch] = useState('')
+  return (
+
+    <>
+    <Form.Control
+    placeholder='Search Player'
+    type='search'
+    className='w-50 m-auto'
+    onChange={(e)=> setSearch(e.target.value)}
+    />
+     <Container className='card-container rounded-4 my-4 p-3'>
+    <Row className='g-3 justify-content-center'>
+        {data.filter((player)=>player.name.toLowerCase().includes(search.trim().toLowerCase())).map((player , i)=> (
+            <Col md={6} lg={4} xl={3} key={i}>
         <PlayerCard {...player}/>
-      </Col>
-    
-    ))}
+            </Col>
+    ) )}
     </Row>
     
-    </Container>
-  ) 
-};
+  </Container>
+    </>
+ 
+  )
+}
 
-export default CardContainer;
+export default CardContainer
--- REACT/r-6-nba-legends/src/components/PlayerCard.jsx
+++ REACT/r-6-nba-legends/src/components/PlayerCard.jsx
@@ -1,14 +1,32 @@
-import React from 'react'
-import {Card} from "react-bootstrap";
+import React from "react";
+import { useState } from "react";
+import { Card } from "react-bootstrap";
 
-const PlayerCard = ({name, img, statistics}) => {
+const PlayerCard = ({ name, img, statistics }) => {
+  const [showImage, setShowImage] = useState(true);
+  const handleClick = () => {
+    setShowImage(!showImage);
+  };
   return (
-    <Card className='rounded-2 m-auto player-card'>
-      <Card.Img variant="top" src={img} className="player-logo" role="button"/>
-      
+    <Card onClick={handleClick} className="rounded-2 m-auto player-card">
+      {showImage ? (
+        <Card.Img
+          variant="top"
+          src={img}
+          className="player-logo"
+          role="button"
+        />
+      ) : (
+        <ul className="m-auto">
+          {statistics.map((item, i) => {
+            return <li className="h5 text-start" key={i}>🏀{item}</li>;
+          })}
+        </ul>
+      )}
+
       <Card.Footer>
-        <Card.Title >{name}</Card.Title>
-       </Card.Footer>
+        <Card.Title>{name}</Card.Title>
+      </Card.Footer>
     </Card>
   );
 };
